3c4c6272eda6c382d5f531cca3ffd59eb0aeef0f
KomeSake <135702705+KomeSake@users.noreply.github.com>
2024-11-17 23:37:00 +0900

    完善了Player的移动
    但是现在跳跃功能还有点问题，还需要修复一下

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody _rb;
+    NWH.DWP2.WaterObjects.WaterObject _waterObject;//水插件脚本
+
+    float _submergedVolume;//浮力
+
+    public float _speed;
+    public float _rotationSpeed;
+    public float _jumpSpeed;
+
+    public bool _isJump;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _waterObject = GetComponent<NWH.DWP2.WaterObjects.WaterObject>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+    private void FixedUpdate()
+    {
+        _submergedVolume = _waterObject.submergedVolume;
+        if (_submergedVolume > 1f)
+            _isJump = false;
+    }
+
+    void Move()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 moveDir = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (moveDir.magnitude > 0.1f)
+        {
+            //旋转
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * _rotationSpeed);
+            //移动
+            _rb.velocity = new(moveDir.x * _speed, _rb.velocity.y, moveDir.z * _speed);
+        }
+        //跳跃
+        if (Input.GetKeyDown(KeyCode.Space) && !_isJump)
+        {
+            _rb.AddForce(_rb.mass * _jumpSpeed * Vector3.up, ForceMode.Impulse);
+            _isJump = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ice"))
+        {
+            _isJump = false;
+        }
+    }
+}
