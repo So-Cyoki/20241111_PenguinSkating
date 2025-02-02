c0dc0bcedd330980f3cedbbafc4860de7fa24901
So-Cyoki <146906786+So-Cyoki@users.noreply.github.com>
2025-02-02 17:32:20 +0900

    添加了海浪的难度机制，还有数值的小修改

--- Assets/Scripts/IceMountainMap.cs
+++ Assets/Scripts/IceMountainMap.cs
@@ -5,31 +5,38 @@ public class IceMountainMap : MonoBehaviour
 {
     //用于下一个地图块的连接点
     public Transform _endPoint;
-    //用于提供给父物体的碰撞位置，开始会将位置和大小信息覆盖给父物体的Collider
-    public Transform _colliderGrid;
-    //父物体上的触发器，用于判断玩家是否经过地图块一半了
-    BoxCollider _collider;
-    bool _isCreateNewMap;//是否已经创建了新的地图块
+    public BoxCollider _collider1;//中间碰撞体
+    public BoxCollider _collider2;//结尾碰撞体
 
     public static event Action<Vector3> OnMoveMiddle;//触发了中间点的碰撞
+    public static event Action OnMoveLast;//触发了结尾点的碰撞
 
-    private void Awake()
+    public void GameRestart()
     {
-        _collider = GetComponent<BoxCollider>();
-    }
-    private void Start()
-    {
-        //将用于展示用的方块的位置和大小赋值给父物体的碰撞体
-        _collider.center = _colliderGrid.localPosition;
-        _collider.size = _colliderGrid.localScale;
+        _collider1.enabled = true;
+        _collider2.enabled = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") && !_isCreateNewMap)
+        if (other.gameObject.CompareTag("Player") && _collider1.enabled)
         {
             OnMoveMiddle?.Invoke(_endPoint.position);
-            _isCreateNewMap = true;
+            _collider1.enabled = false;
         }
+        else if (other.gameObject.CompareTag("Player") && _collider2.enabled)
+        {
+            OnMoveLast?.Invoke();
+            _collider2.enabled = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        SeaWave.OnGameOver += GameRestart;
+    }
+    private void OnDisable()
+    {
+        SeaWave.OnGameOver -= GameRestart;
     }
 }
--- Assets/Scripts/Manager/IceMountainManager.cs
+++ Assets/Scripts/Manager/IceMountainManager.cs
@@ -12,7 +12,36 @@ public class IceMountainManager : MonoBehaviour
     [Header("难度增加")]
     public SeaWave _seaWaveObj;
     public float _addWaveSpeed;
+    public Crest.ShapeFFT _shapeFFT;//海浪脚本
+    public float _addShapeWeight;
+    public float _addShapeWind;
+    public float _shapeLerpSpeed;
+    float _shapeWeight_original;
+    float _shapeWind_original;
+    float _shapeWeight_target;
+    float _shapeWind_target;
 
+    int _diffcultyCount = 1;//当前难度水平
+
+    private void Start()
+    {
+        _shapeWeight_original = _shapeFFT._weight;
+        _shapeWind_original = _shapeFFT._windTurbulence;
+        _shapeWeight_target = _shapeFFT._weight;
+        _shapeWind_target = +_shapeFFT._windTurbulence;
+    }
+    private void LateUpdate()
+    {
+        //逐步变大波浪
+        if (_shapeWeight_target != _shapeFFT._weight)
+        {
+            _shapeFFT._weight = Mathf.Lerp(_shapeFFT._weight, _shapeWeight_target, _shapeLerpSpeed);
+        }
+        if (_shapeWind_target != _shapeFFT._windTurbulence)
+        {
+            _shapeFFT._windTurbulence = Mathf.Lerp(_shapeFFT._windTurbulence, _shapeWind_target, _shapeLerpSpeed);
+        }
+    }
     void CreatNewMap(Vector3 startPos)
     {
         //按照事件的广播，随机一个新的地图块并按照传入的坐标拼接
@@ -22,8 +51,7 @@ public class IceMountainManager : MonoBehaviour
         //储存新建的地图块，用以一会删除
         _waitDestoryMap.Add(newMap);
 
-        //增加难度，让海浪加速
-        _seaWaveObj._lerpSpeed += _addWaveSpeed;
+        DiffcultyAdd();//难度增加
     }
     public void ResetMap()
     {
@@ -33,14 +61,56 @@ public class IceMountainManager : MonoBehaviour
             Destroy(item);
         }
         _waitDestoryMap.Clear();
+
+        //重置难度
+        _diffcultyCount = 1;
+        _shapeFFT._weight = _shapeWeight_original;
+        _shapeFFT._windTurbulence = _shapeWind_original;
+        _shapeWeight_target = _shapeFFT._weight;
+        _shapeWind_target = +_shapeFFT._windTurbulence;
+    }
+    void DiffcultyAdd()
+    {
+        switch (_diffcultyCount)
+        {
+            case 1:
+                _shapeWeight_target += _addShapeWeight;
+                break;
+            case 2:
+                _seaWaveObj._lerpSpeed += _addWaveSpeed;
+                _shapeWeight_target += _addShapeWeight;
+                break;
+            case 3:
+                _shapeWeight_target += _addShapeWeight * 0.5f;
+                _shapeWind_target += _addShapeWind;
+                break;
+            case 4:
+                _seaWaveObj._lerpSpeed += _addWaveSpeed;
+                _shapeWeight_target += _addShapeWeight * 0.5f;
+                _shapeWind_target += _addShapeWind * 0.5f;
+                break;
+            case 5:
+                _seaWaveObj._lerpSpeed += _addWaveSpeed;
+                _shapeWeight_target += _addShapeWeight * 0.5f;
+                _shapeWind_target += _addShapeWind * 0.5f;
+                break;
+        }
+        if (_shapeWeight_target > 1)
+            _shapeWeight_target = 1;
+        if (_shapeWind_target > 1)
+            _shapeWind_target = 1;
+
+        _diffcultyCount++;//难度+1
     }
 
     private void OnEnable()
     {
         IceMountainMap.OnMoveMiddle += CreatNewMap;
+        IceMountainMap.OnMoveLast += DiffcultyAdd;
     }
     private void OnDisable()
     {
         IceMountainMap.OnMoveMiddle -= CreatNewMap;
+        IceMountainMap.OnMoveLast -= DiffcultyAdd;
     }
 }
